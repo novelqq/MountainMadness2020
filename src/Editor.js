c9d170460a244a1580f64ab5c4cf1543babba7e6
Michael Bennett <michaelbennett.dev@gmail.com>
2020-02-22 16:38:49 -0800

    Add things for ejection

--- src/Editor.js
+++ src/Editor.js
@@ -1,50 +1,44 @@
-import React from 'react';
-import PiScroller from './Scroller'
-import './Editor.css';
+import React from "react";
+import PiScroller from "./Scroller";
+import "./Editor.css";
 
 class PiEditor extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                inputText: '',
-                arrow: 0
-            };
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: "",
+      arrow: 0
+    };
+  }
 
-        render() {
-            const style = {
-                color: 'blue',
-                width: '100%',
-                height: '100%',
-                backgroundColor: 'blue',
-            };
+  render() {
+    const style = {
+      color: "black",
+      width: "100%",
+      height: "100%",
+      backgroundColor: "white"
+    };
 
-            return ( < div className = 'pi-editor'
-                style = {
-                    style
-                } > < div style = {
-                    style
-                }
-                className = 'edit-view' >
-                <
-                input style = {
-                    style
-                }
-                value = {
-                    this.state.inputText
-                }
-                onChange = {
-                    txt => this.update_view(txt)
-                }
-                /></div > < PiScroller / > < /div>);
-            }
+    return (
+      <div className="pi-editor" style={style}>
+        <div style={style} className="edit-view">
+          <input
+            style={style}
+            value={this.state.inputText}
+            onChange={txt => this.update_view(txt)}
+          />
+        </div>
+        <PiScroller />
+      </div>
+    );
+  }
 
-            update_view(txt) {
-                console.log(txt.target.value);
-                this.setState({
-                    inputText: txt.target.value
-                });
-            }
-        }
+  update_view(txt) {
+    console.log(txt.target.value);
+    this.setState({
+      inputText: txt.target.value
+    });
+  }
+}
 
-        export default PiEditor;
+export default PiEditor;
--- src/Scroller.js
+++ src/Scroller.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React from "react";
+/*import AppleScrollWheel from "react-native-apple-scroll-wheel";*/
 
-class PiScroller extends React.Component
-{
-    render() {
-        return (<div/>);
-    }
+class PiScroller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { scrollValue: 0 };
+  }
+
+  render() {
+    /*return (<AppleScrollWheel
+      value={this.state.scrollValue}
+      minValue={-720}
+      increment={1}
+      maxValue={720}
+      onChangeValue={scrollValue => this.setState({ scrollValue })}
+    />);*/
+    return (
+      <div
+        className="tape"
+        style={{
+          willChange: "transform",
+          position: "absolute",
+          left: 0,
+          bottom: 0,
+                color: 'black',
+          transform: `translate3d(${this.state.scrollValue}px, 0, 0)`
+        }}
+      >hello world</div>
+    );
+  }
 }
 
 export default PiScroller;
